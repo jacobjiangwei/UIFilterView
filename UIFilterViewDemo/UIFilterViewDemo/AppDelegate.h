0d66463cae87a4075346853a3a12cbf8b13f535c
jacob <jacob@jiangwei.local>
2013-05-22 09:57:02 +0800

    first commit

---
+++ UIFilterViewDemo/UIFilterViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIFilterViewDemo
+//
+//  Created by jacob on 13-5-22.
+//  Copyright (c) 2013年 jacob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
